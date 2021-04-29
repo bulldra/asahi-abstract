62dbbb83cdbaac9b0b2bad9c26f5ed9b4cf60b52
bulldra <bullda@gmail.com>
2021-04-29 14:48:08 +0900

    設定ファイルパス生成方法の修正

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ import os
 import json
 
 def build_setting_path(path):
-    return os.path.dirname(os.path.abspath(__file__)) + '/' + path
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
 
 def load_json_setting(path):
     path = open(build_setting_path(path), 'r')
